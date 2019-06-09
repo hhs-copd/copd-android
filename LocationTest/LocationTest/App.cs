60f8602d94c107c1d6d90c676f1e9fdb4e9e5855
Stefan van der Sman <sj.vandersman@gmail.com>
2019-06-09 23:31:02 +0200

    Fix merge, ups

--- LocationTest/LocationTest/App.cs
+++ LocationTest/LocationTest/App.cs
@@ -1,13 +1,41 @@
-﻿using LocationTest.Pages;
+﻿using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LocationTest.Views;
+using LocationTest.ViewModels.Base;
+using LocationTest.Pages;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace LocationTest
 {
-    public class App : Application
+    public partial class App : Application
     {
+        private static ViewModelLocator _locator;
+
+        public static ViewModelLocator Locator
+        {
+            get { return _locator = _locator ?? new ViewModelLocator(); }
+        }
+
         public App()
         {
-            this.MainPage = new LoginPage();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
         }
     }
 }
