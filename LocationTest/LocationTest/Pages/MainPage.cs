8b4ab3dc40e1a1756633b1c0a4493b04015c6400
Stefan van der Sman <sj.vandersman@gmail.com>
2019-06-11 17:41:15 +0200

    Add broken upload button

--- LocationTest/LocationTest/Pages/MainPage.cs
+++ LocationTest/LocationTest/Pages/MainPage.cs
@@ -2,6 +2,7 @@
 using LocationTest.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Xamarin.Forms;
 
 namespace LocationTest.Pages
@@ -11,6 +12,8 @@ namespace LocationTest.Pages
         private readonly StackLayout ConnectedDevicesLayout = new StackLayout();
 
         private readonly IBluetoothService BluetoothService;
+
+        private readonly ILambdaFunctionDataService LambdaFunctionDataService;
 
         private readonly LoginResult LoginResult;
 
@@ -24,6 +27,7 @@ namespace LocationTest.Pages
             DependencyService.Register<ILambdaFunctionDataService>();
 
             this.BluetoothService = DependencyService.Get<IBluetoothService>();
+            this.LambdaFunctionDataService = DependencyService.Get<ILambdaFunctionDataService>();
 
             this.BluetoothService.Listen(new BluetoothHandler
             {
@@ -77,18 +81,48 @@ namespace LocationTest.Pages
             buttonGrid.Children.Add(buttonPlot);
             Grid.SetColumn(buttonPlot, 1);
 
+            Button buttonUpload = new Button
+            {
+                Text = "Upload data",
+                HorizontalOptions = LayoutOptions.Center,
+                IsEnabled = true,
+                IsVisible = true,
+                Margin = new Thickness(00, 10, 0, 0),
+                Padding = new Thickness(32, 5)
+            };
+            buttonUpload.Clicked += ButtonUpload_Clicked;
             this.Padding = new Thickness(5, Device.RuntimePlatform == Device.iOS ? 20 : 0, 5, 0);
             this.Content = new ScrollView
             {
                 Content = new StackLayout()
                 {
                     Children = {
+                        buttonUpload,
                         buttonGrid,
                         connectedDevicesLabel,
                         this.ConnectedDevicesLayout
                     }
                 }
             };
+        }
+
+        private async void ButtonUpload_Clicked(object sender, EventArgs e)
+        {
+            string from = Path.Combine("storage", "emulated", "0", "Android", "data", "com.copd.COPDMonitor.Android", "files", "data.csv");
+            string to = Path.Combine("storage", "emulated", "0", "Android", "data", "com.copd.COPDMonitor.Android", "files", "upload.csv");
+            File.Copy(from, to);
+            File.Delete(from);
+            try{
+
+                await this.LambdaFunctionDataService.PostData(this.LoginResult.AccessToken, to);
+
+                System.IO.File.Delete(to);
+            }
+            catch (Exception exept)
+            {
+                //De button is nu helemaal waardeloos, ik kijk er later na
+                Console.WriteLine(exept);
+            }
         }
 
         private async void OnConnect(object sender, EventArgs e)
