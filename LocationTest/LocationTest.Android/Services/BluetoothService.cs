a6c3b9b00f8d87fe6ba36cbd2df0e5287d4d417e
Christian Ruigrok <christian@ruigrok.info>
2019-06-13 12:29:47 +0200

    Fix multi write to file

--- LocationTest/LocationTest.Android/Services/BluetoothService.cs
+++ LocationTest/LocationTest.Android/Services/BluetoothService.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -42,16 +43,48 @@ namespace LocationTest.Droid.Services
             return CrossBluetoothLE.Current.Adapter.ConnectedDevices.Select(device => device.Name).ToList();
         }
 
+        private static void ClearEventInvocations(object obj, string eventName)
+        {
+            FieldInfo fi = GetEventField(obj.GetType(), eventName);
+            if (fi == null)
+            {
+                return;
+            }
+
+            fi.SetValue(obj, null);
+        }
+
+        private static FieldInfo GetEventField(Type type, string eventName)
+        {
+            FieldInfo field = null;
+            while (type != null)
+            {
+                /* Find events defined as field */
+                field = type.GetField(eventName, BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                if (field != null && (field.FieldType == typeof(MulticastDelegate) || field.FieldType.IsSubclassOf(typeof(MulticastDelegate))))
+                {
+                    break;
+                }
+
+                /* Find events defined as property { add; remove; } */
+                field = type.GetField("EVENT_" + eventName.ToUpper(), BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
+                if (field != null)
+                {
+                    break;
+                }
+
+                type = type.BaseType;
+            }
+            return field;
+        }
+
         public void Listen(BluetoothHandler handler = null)
         {
-           
-                               IBluetoothLE bluetooth = CrossBluetoothLE.Current;
+
+            IBluetoothLE bluetooth = CrossBluetoothLE.Current;
             bluetooth.Adapter.ScanMode = ScanMode.Balanced;
             bluetooth.Adapter.DeviceDiscovered += (device, args) =>
-            {             
-                   
-                
-               
+            {
                 ReadData(bluetooth.Adapter, args, handler?.OnConnect);
             };
             bluetooth.Adapter.DeviceDisconnected += (_, args) => handler?.OnDisconnect?.Invoke(args.Device.Name);
@@ -63,7 +96,7 @@ namespace LocationTest.Droid.Services
 
         private static async void ReadData(IAdapter adapter, DeviceEventArgs args, Action<string> onConnect = null)
         {
-          
+
             try
             {
                 if (!AllowedDevices.Any(device => args.Device.NativeDevice.ToString().StartsWith(device)))
@@ -76,11 +109,8 @@ namespace LocationTest.Droid.Services
 
                 onConnect(args.Device.Name);
 
-               
-
                 foreach (IDevice dev in adapter.ConnectedDevices)
                 {
-                  
                     IList<IService> services = await dev.GetServicesAsync();
                     IService relevantService = services.FirstOrDefault(x => x.Id.Equals(Guid.Parse(ServiceGuid)));
                     IList<ICharacteristic> characteristics = await relevantService.GetCharacteristicsAsync();
@@ -98,10 +128,11 @@ namespace LocationTest.Droid.Services
                     {
                         File.Create(filePath).Dispose();
                     }
-                    
+
+                    ClearEventInvocations(read, "ValueUpdated");
                     read.ValueUpdated += (o, _) =>
                     {
-                        
+
                         byte[] data = read.Value;
                         using (FileStream stream = new FileStream(filePath, FileMode.Append))
                         {
